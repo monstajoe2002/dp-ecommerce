5c34c13cb7411ff91b67817885f50ce25ed52e6f
Youssefporsche <youssef.ahmed10computet@gmail.com>
2022-12-22 14:12:40 -0800

    end

--- backend/main.ts
+++ backend/main.ts
@@ -1,4 +1,9 @@
+import { FactoryType } from "./enums/FactoryType";
+import FactoryOrchestrator from "./Factory_Classes/FactoryOrchesterator";
+import DairyFactory from "./Factory_Classes/DairyFactory";
 
+const groceriesFactory = new FactoryOrchestrator().createFactory(FactoryType.DairyGrocery)
+console.log(groceriesFactory);
 
 // class AbstractFactoryDemo {
 //     main() {
