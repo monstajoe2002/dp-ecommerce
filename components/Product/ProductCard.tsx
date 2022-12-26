b0aa12325949f186e844a72d15a81a1c17f7644a
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-12-26 19:43:36 +0200

    seller delete product action

--- components/Product/ProductCard.tsx
+++ components/Product/ProductCard.tsx
@@ -15,16 +15,27 @@ import {
   Link,
   Box,
   HStack,
+  IconButton,
 } from "@chakra-ui/react";
 import { Product } from "../../types/Product";
 import useCart from "../../hooks/useCart";
 import { CartItem } from "../../types/CartItem";
 import { AuthContext } from "../../context/authContext";
 import { CloseIcon, EditIcon } from "@chakra-ui/icons";
+import axios from "axios";
 export default function ProductCard({ product }: { product: Product }) {
   const { addItem } = useCart();
   const toast = useToast();
   const { user } = useContext(AuthContext);
+  const deleteProduct = async () => {
+    toast({
+      title: "Deleted",
+      description: "Product deleted",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
   const addToCart = (): void => {
     if (!user.username) {
       toast({
@@ -48,51 +59,59 @@ export default function ProductCard({ product }: { product: Product }) {
 
   return (
     <>
-      <Link href={`/product/${product.id}`} _hover={{ textUnderline: "none" }}>
-        <Card maxW={"sm"}>
-          <CardBody>
-            <Stack>
-              {user.role === "seller" ? (
-                <>
-                  <HStack ml={"auto"} spacing={4}>
-                    <EditIcon boxSize={5} />
-                    <CloseIcon />
-                  </HStack>
-                </>
-              ) : null}
-              <Center>
-                <Image
-                  boxSize="250px"
-                  objectFit="cover"
-                  src={product.image}
-                  alt={product.title}
-                  borderRadius="lg"
-                />
-              </Center>
-              <Heading size="md">{product.title}</Heading>
-              <Text>{product.description}</Text>
-              <Text fontSize="2xl" textColor="blue.600">
-                {new Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                }).format(product.price)}
-              </Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter
-            style={{
-              display: user.role === "seller" ? "none" : "block",
-            }}
-          >
-            <ButtonGroup spacing="2">
-              <Button variant="solid" colorScheme="blue" onClick={addToCart}>
-                Add to cart
+      <Card maxW={"sm"}>
+        <CardBody>
+          <Stack>
+            {user.role === "seller" ? (
+              <>
+                <HStack ml={"auto"} spacing={4} mb={4}>
+                  <IconButton aria-label="edit product" icon={<EditIcon />} />
+                  <IconButton
+                    aria-label="edit product"
+                    icon={<CloseIcon />}
+                    colorScheme={"red"}
+                    onClick={deleteProduct}
+                  />
+                </HStack>
+              </>
+            ) : null}
+            <Center>
+              <Image
+                boxSize="250px"
+                objectFit="cover"
+                src={product.image}
+                alt={product.title}
+                borderRadius="lg"
+              />
+            </Center>
+            <Heading size="md">{product.title}</Heading>
+            <Text>{product.description}</Text>
+            <Text fontSize="2xl" textColor="blue.600">
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(product.price)}
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          {user.role === "seller" ? (
+            <Link
+              href={`/product/${product.id}`}
+              _hover={{ textUnderline: "none" }}
+            >
+              <Button variant="solid" colorScheme="blue">
+                Review
               </Button>
-            </ButtonGroup>
-          </CardFooter>
-        </Card>
-      </Link>
+            </Link>
+          ) : (
+            <Button variant="solid" colorScheme="blue" onClick={addToCart}>
+              Add to cart
+            </Button>
+          )}
+        </CardFooter>
+      </Card>
     </>
   );
 }
