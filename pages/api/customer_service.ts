9b4d18f401c69bf555f7d4ccce77e1c32a8376f6
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-12-27 12:51:16 +0200

    customer service 👍

--- pages/api/customer_service.ts
+++ pages/api/customer_service.ts
@@ -4,14 +4,8 @@ import { CustomerService } from "../../backend/Users/Customer_Service";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    const customerService = new CustomerService(
-      req.body.username,
-      req.body.password
-    );
-    res
-      .status(200)
-      .json({
-        message: `Customer Service created successfully: ${customerService}`,
-      });
+    res.status(200).json({
+      message: `Customer Service created successfully. We have received the following complaint: ${req.query.complaint}.`,
+    });
   }
 }
