42338b7a54c4037397f8f656eb0adf541e051505
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-12-27 12:37:28 +0200

    modal

--- backend/main.ts
+++ backend/main.ts
@@ -1,16 +1,18 @@
 import { FactoryType } from "./enums/FactoryType";
 import {FactoryOrchestrator} from "./Factory_Classes/FactoryOrchestrator";
 import DairyFactory from "./Factory_Classes/DairyFactory";
+import { DairyType } from "./enums/DairyType";
 
 const groceriesFactory = new FactoryOrchestrator().createFactory(FactoryType.DairyGrocery)
-console.log(groceriesFactory);
+const cheese = groceriesFactory.createProduct(DairyType.Cheese as never);
+console.log(cheese);
 
-class AbstractFactoryDemo {
-    main() {
-       // create instance of factory
-      const factoryProducer = new FactoryOrchestrator();
+// class AbstractFactoryDemo {
+//     main() {
+//       //  create instance of factory
+//       const factoryProducer = new FactoryOrchestrator();
     
 
-    }
-}
-AbstractFactoryDemo.prototype.main();
+//     }
+// }
+// AbstractFactoryDemo.prototype.main();
--- components/Product/EditProductModal.tsx
+++ components/Product/EditProductModal.tsx
@@ -5,29 +5,115 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
-  useDisclosure,
   ModalFooter,
   Button,
+  FormControl,
+  Input,
+  FormLabel,
+  Select,
+  useToast,
+  Stack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import { FactoryType } from "../../backend/enums/FactoryType";
 
-export default function EditProductModal({isOpen, onClose}: {isOpen: boolean, onClose: () => void}) {
+export default function EditProductModal({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  const name = React.useRef<HTMLInputElement>(null);
+  const price = React.useRef<HTMLInputElement>(null);
+  const description = React.useRef<HTMLInputElement>(null);
+  const img = React.useRef<HTMLInputElement>(null);
+  const stockQuantity = React.useRef<HTMLInputElement>(null);
+  const category = React.useRef<HTMLSelectElement>(null);
+  const [image, setImage] = useState<File | null>(null);
+  const [createObjectURL, setCreateObjectURL] = useState<string | null>(null);
+  const toast = useToast();
+  const handleSubmit = () => {
+    const product = {
+      name: name.current?.value,
+      price: price.current?.value,
+      description: description.current?.value,
+      img: img.current?.value,
+      stockQuantity: stockQuantity.current?.value,
+      category: category.current?.value,
+    };
+    if (!Object.values(product).every((v) => v)) {
+      toast({
+        title: "Error",
+        description: "Please fill in all fields",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+    console.log(product);
+  };
+  const uploadToClient = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      const i = event.target.files[0];
+      setImage(i);
+      setCreateObjectURL(URL.createObjectURL(i));
+    }
+  };
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>Edit Product</ModalHeader>
           <ModalCloseButton />
-                  <ModalBody>
-                      
-          </ModalBody>
+          <Stack gap={4}>
+            <ModalBody>
+              <FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Name</FormLabel>
+                  <Input ref={name} placeholder="Name" />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Price</FormLabel>
+                  <Input ref={price} type={"number"} placeholder="Price" />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Description</FormLabel>
+                  <Input ref={description} placeholder="Description" />
+                </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Stock Quantity</FormLabel>
+                  <Input ref={stockQuantity} placeholder="Stock Quantity" />
+                </FormControl>
+                <Select ref={category} placeholder="Select category" isRequired>
+                  {Object.values(FactoryType).map((type) => (
+                    <option key={type} value={type}>
+                      {type}
+                    </option>
+                  ))}
+                </Select>
+                <FormControl>
+                  <FormLabel>Upload image</FormLabel>
+                  <Input
+                    ref={img}
+                    type="file"
+                    variant={"filled"}
+                    p={0}
+                    boxSize={"auto"}
+                    onChange={uploadToClient}
+                  />
+                </FormControl>
+              </FormControl>
+            </ModalBody>
+          </Stack>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
               Save
             </Button>
-            <Button variant="ghost">Cancel</Button>
+            <Button variant="ghost" onClick={onClose}>Cancel</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
